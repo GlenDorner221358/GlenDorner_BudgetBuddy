649f6e1f650de1e34dc8904eb50631c6579e92f8
GlenDorner221358 <221358@virtualwindow.co.za>
2024-04-05 18:15:21 +0200

    Component testing baseline
    Added the boilerplate for component testing code

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+export default {
+    collectCoverage: true, //toggle to true to export coverage report
+    collectCoverageFrom: ['src/**/*.{js,jsx}'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+    }
+}
